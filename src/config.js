01e0c4beb82b4a4e3e89023090c7a36821c682b4
codixir <codixir@gmail.com>
2020-07-14 18:29:44 -0400

    Setup react

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "",
+    BUCKET: "notes-my-files-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL:"https://bzy3sobcmc.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ca-central-1",
+    USER_POOL_ID:"ca-central-1_dlunXIi22",
+    APP_CLIENT_ID:"47vam4mq37u3p2pe69e240irh1",
+    IDENTITY_POOL_ID: "ca-central-1:8028c6f4-bb92-4c65-b623-8e524f3b7124"
+  }
+}
